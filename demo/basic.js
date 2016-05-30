201345d56d3d6d760f4e08535de70dbd379d081c
suewonjp <suewonjp@gmail.com>
2016-05-30 10:16:39 +0900

    [update] added more comment for demo code

--- demo/basic.js
+++ demo/basic.js
@@ -52,9 +52,10 @@ function createBasicDemoScene($) {
         dndx("#draggable0", ".row1").newPair("#draggable0", "#basic-dialog p").newPair("#draggable1", "#basic-dialog p");
 
         // We need to let the entire frame of the dialog interact with source objects even though the frame doesn't need to accept them.
-        // Without this invisible interaction, some hidden target objects behind the dialog may accept a source object where the users cannot notice it, and most of time, this behavior will surprise the users!
+        // Without this invisible interaction, some hidden target objects behind the dialog may accept the source object where the users cannot notice it, and most of time, this behavior will surprise the users!
         // .nullify() API is for dealing with this kind of issue
-        // But notice that just calling .nullify() is not enough. See the CONFLICT EVENT HANDLER above
+        // But notice that just calling .nullify() is not enough. See the CONFLICT EVENT HANDLER below
+        // Read https://github.com/suewonjp/DNDX.JS/wiki/DNDX.JS-Topic---Drag-&-Drop-Conflict-Resolution
         dndx("#draggable0", ".ui-dialog").nullify().newPair("#draggable1", ".ui-dialog");
     });
 
@@ -67,8 +68,8 @@ function createBasicDemoScene($) {
 
     $(".check-to-reject-row")
         .on("click", function(e) {
-            // these checkboxes are to make some targets enable or disable temporally.
-            // See PAIR CHECKING EVENT HANDLER above
+            // these checkboxes are to make some targets enable or disable temporarily.
+            // See PAIR CHECKING EVENT HANDLER below
             var $btn = $(e.target);
             if ($btn.prop("checked")) {
                 $btn.closest("tr").find("div.droppable").addClass("blocked");
@@ -91,37 +92,43 @@ function createBasicDemoScene($) {
 }
 
 function enterBasicDemoScene($, $panel) {
-    //function onOver(eventType, $srcObj, $tgtObj) {}
-    //function onOut(eventType, $srcObj, $tgtObj) {}
-
     //
     // DROP EVENT HANDLER
     //
     // -- This will be called as soon as a source object is dropped on a target object
+    //
     function onDrop(eventType, $srcObj, $tgtObj) {
         if ($tgtObj.is(".ui-dialog") === false)
             $tgtObj.animateCss("bounce");
-            //$tgtObj.effect("bounce", "slow");
     }
 
     //
     // CONFLICT EVENT HANDLER
     //
+    // -- Will be called whenever two objects are competing for interaction with the source
+    //
     function onConflict($srcObj, $tgtObj0, $tgtObj1) {
         var arr = [ $tgtObj0, $tgtObj1, ], i;
         for (i=0; i<2; ++i) {
+            // We give highest priority for the <p> element on the dialog
             if (arr[i].is("#basic-dialog p"))
                 return arr[i];
         }
         for (i=0; i<2; ++i) {
+            // We give the 2nd highest for the dialog itself 
             if (arr[i].is(".ui-dialog"))
                 return arr[i];
         }
+        // "First come, first served" for others
         return $tgtObj0;
     }
 
     //
     // PAIR CHECKING EVENT HANDLER
+    //
+    // -- Will be called whenever a pair of (source/target) objects is detected.
+    // -- Your job is determining whether to send it down for subsequent events.
+    // -- Useful when you want to temporarily rule out some particular pairs 
     //
     function onCheckPair($srcObj, $tgtObj, srcSelector, tgtSelector) {
         // Ignore any of elements having the following class name.
@@ -132,13 +139,12 @@ function enterBasicDemoScene($, $panel) {
     // SETTINGS FOR THE GLOBAL LEVEL
     //
     // -- These settings will affect every pair
+    //
     dndx()
         //.draggableOptions({ helper: "clone", })
         .visualcue($("#basic-visualcue-menu")[0].selectedOptions[0].text)
         .oncheckpair(onCheckPair)
         .onconflict(onConflict)
-        //.onover(onOver)
-        //.onout(onOut)
         .ondrop(onDrop);
 
     var dlg = $("#basic-dialog");
@@ -155,8 +161,6 @@ function leaveBasicDemoScene($, $panel) {
         .visualcue(null)
         .oncheckpair(null)
         .onconflict(null)
-        //.onover(null)
-        //.onout(null)
         .ondrop(null);
 
     var dlg = $("#basic-dialog"), opened = dlg.dialog("isOpen");
--- demo/list.js
+++ demo/list.js
@@ -2,9 +2,17 @@
 /*eslint no-unused-vars:0*/
 
 function createListDemoScene($) {
+    // jQuery object representing the trash can
     var $trashcan = $(".trashcan-container .fa").tooltip().tooltip("disable");
 
+    //
+    // Various helper function for the sortable list.
+    //
     var listHelper = {
+        //
+        // Figure out the index at which the source list item is supposed to be inserted.
+        // It is computed by the current position of the source relative to the list container
+        //
         getInsertablePosition : function($srcObj, $listContainer) {
             var items = $listContainer.children(), result = items.length;
             if (items.length) { // When <ul> or <ol> has <li> items
@@ -27,6 +35,9 @@ function createListDemoScene($) {
             }
             return result;
         },
+        //
+        // Make the list items look thiner so that they fit gaps between the items
+        //
         elongateSize : function($srcObj, elongate) {
             var orgnRc = $srcObj.data("dndx-list-original-src-rect");
             if (elongate) {
@@ -42,11 +53,17 @@ function createListDemoScene($) {
                 });
             }
         },
+        //
+        // A handler called when the source starts dragging ( "dragstart" event )
+        //
         onStart : function (eventType, $srcObj, $tgtObj, etc) {
             var w = $srcObj.width(), h = $srcObj.height(),
                 rc = $srcObj[0].getBoundingClientRect(), left = rc.left, top = rc.top;
             $srcObj.data("dndx-list-original-src-rect", { left:left, top:top, w:w, h:h, });
         },
+        //
+        // A handler called when the source stops dragging ( "dragstop" event )
+        //
         onDrop : function (eventType, $srcObj, $tgtObj) {
             var idx = listHelper.getInsertablePosition($srcObj, $tgtObj), items = $tgtObj.children();
             if (idx === items.length) {
@@ -57,6 +74,9 @@ function createListDemoScene($) {
             }
             $srcObj.css({ top:"", left:"", });
         },
+        //
+        // A custom visucal cue callback for the list items.
+        //
         visualcue : function (eventType, $srcObj, $tgtObj) {
             switch (eventType) {
             case "dropactivate": 
@@ -66,11 +86,16 @@ function createListDemoScene($) {
                 $tgtObj.removeClass("dndx-visualcue-exterior-aqua");
                 break;
             case "dropover": 
+                // Make list items look thiner whenever they interact with the list container.
+                // Also, give some visual cue to the container, too.
                 $tgtObj.addClass("dndx-visualcue-interior-red").css("min-height", $tgtObj.height());
                 listHelper.elongateSize($srcObj, true);
                 break;
             case "dropout":
             case "drop":
+                // Make back list items to the original size when they get dragged out of the list container
+                // or they finally get dropped to the container.
+                // Also, remove the visual cue from the container
                 listHelper.elongateSize($srcObj, false);
                 $tgtObj.removeClass("dndx-visualcue-interior-red").css("min-height", null);
                 break;
@@ -78,16 +103,21 @@ function createListDemoScene($) {
         },
     }; 
 
+    //
+    // Drop event handler for the trash can.
+    //
     function trashListItem(eventType, $srcObj, $tgtObj) {
         $srcObj
             .draggable("option", "revert", false)
             .animateCss("zoomOutDown", function() {
+                // Enable the tooltip message
                 $trashcan.tooltip("enable")
                     .tooltip("option", "content", "You really hate "+$(this).text() + " so much...")
                     .tooltip("open")
                     .animateCss("rubberBand")
                     ;
 
+                // Disable the tooltip a little later
                 setTimeout(function() {
                     $trashcan.tooltip("close").tooltip("disable");
                 }, 2000);
@@ -96,13 +126,18 @@ function createListDemoScene($) {
             });
     }
 
+    //
+    // Settings for the list items.
+    // 
     dndx(".pl-list-container li")
         .draggableOptions({ revert: true, })
         .onstart(listHelper.onStart)
         .targets(".pl-list-container ul, .pl-list-container ol")
+            // Configurations for the case that the target is one of list containers
             .visualcue(listHelper.visualcue)
             .ondrop(listHelper.onDrop)
         .targets(".trashcan-container .fa")
+            // Configurations for the case that the target is the trash can icon
             .visualcue("Arrow")
             .ondrop(trashListItem)
         ;
